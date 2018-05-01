04486dcc49983995b91f54431f5af342da47c3c7
Nicholas Belli <nicholas.belli@gmail.com>
2018-05-01 16:12:40 -0400

    Two methods written

---
+++ DapperWebAPIDemo/DapperWebAPIDemo/DAL/CustomerRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+using DapperWebAPIDemo.Models;
+
+
+namespace DapperWebAPIDemo.DAL
+{    
+    public class CustomerRepository : ICustomerRepository
+    {
+        private IDbConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+
+        public List<Customer> GetCustomers(int amount, string sort)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetSingleCustomer(int customerId)
+        {
+            return connection.Query<Customer>("SELECT[CustomerID],[CustomerFirstName],[CustomerLastName],[IsActive]" +
+                                            " FROM [Customer] WHERE CustomerID =@CustomerID", new { CustomerID = customerId }).SingleOrDefault();
+        }
+
+        public bool InsertCustomer(Customer ourCustomer)
+        {
+            int rowsAffected = this.connection.Execute(@"INSERT Customer([CustomerFirstName],[CustomerLastName],[IsActive]) values (@CustomerFirstName, @CustomerLastName,@IsActive)",
+                                                        new { CustomerFirstName = ourCustomer.CustomerFirstName, CustomerLastName = ourCustomer.CustomerLastName, IsActive = true });
+
+            if (rowsAffected > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool DeleteCustomer(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCustomer(Customer ourCustomer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
